5bbf0048f2582da0510778124bdfa4f8a62a0548
liukai <liukai@thfund.com.cn>
2017-01-07 04:20:27 +0800

    UI完成

--- console/src/app/api-editor/api-editor.component.ts
+++ console/src/app/api-editor/api-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+
 @Component({
   selector: 'app-api-editor',
   templateUrl: './api-editor.component.html',
@@ -10,6 +13,13 @@ export class ApiEditorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    CodeMirror.fromTextArea(<HTMLTextAreaElement>document.getElementById('json'), {
+      lineNumbers: true,
+      mode: {
+        name: 'javascript',
+        json: true
+      }
+    });
   }
 
 }
--- console/src/app/app.module.ts
+++ console/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PlaygroundListComponent } from './playground-list/playground-list.component';
